08412aaec253cd0383348df1ac46c89d0417faeb
shashika nuwan <kumararanaweera1999@gmail.com>
2022-01-12 17:26:16 +0530

    first commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shashika',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'frontend';
+
+  ngOnInit() {
+    console.log('hi')
+  }
+}
