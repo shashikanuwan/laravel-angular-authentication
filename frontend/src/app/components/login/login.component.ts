02682222ee391f51ce60073f0916e3e5990fb5e3
shashika nuwan <kumararanaweera1999@gmail.com>
2022-01-13 00:07:56 +0530

    angular login view and form create

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+    email: null,
+    password: null
+  }
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    return this.http.post('http://127.0.0.1:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error),
+    );
+  }
 
   ngOnInit(): void {
   }
